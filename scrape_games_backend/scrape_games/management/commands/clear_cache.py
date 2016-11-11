fbbd14b01db17cfde69fc9c7e69eea9e1e547384
Giulio <giulio@debian.debian>
2016-11-11 11:33:22 +0100

    Add Management command to clear cache data from db. Add cache model to make it easier

---
+++ scrape_games_backend/scrape_games/management/commands/clear_cache.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.utils import timezone
+
+
+from scrape_games.models import MyCacheTable
+
+class Command(BaseCommand):
+    help = 'Clears expired cache pages from database'
+
+    def handle(self, *args, **options):
+        expired_pages = [ page for page in MyCacheTable.objects.all() if page.expires < timezone.now()]
+        i = 0
+        for page in expired_pages:
+            page.delete()
+            i+=1
+            self.stdout.write('Removed expired cached page:'+str(i))
+        self.stdout.write('Done.')
+
--- scrape_games_backend/scrape_games/models.py
+++ scrape_games_backend/scrape_games/models.py
@@ -1,3 +1,12 @@
+from __future__ import unicode_literals
+
 from django.db import models
 
-# Create your models here.
+
+class MyCacheTable(models.Model):
+    cache_key = models.CharField(primary_key=True, max_length=255)
+    value = models.TextField()
+    expires = models.DateTimeField()
+
+    class Meta:
+        db_table = 'my_cache_table'
