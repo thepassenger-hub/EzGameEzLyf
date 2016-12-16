f04e3c87a585e984f08624504110ea422ac35cc6
Giulio <giulio@debian.debian>
2016-12-16 16:07:31 +0100

    Add filter backend

--- scrape_games_backend/scrape_games_frontend/static/js/loadpage-jquery.js
+++ scrape_games_backend/scrape_games_frontend/static/js/loadpage-jquery.js
@@ -1,11 +1,32 @@
 $(document).ready(function(){
 //    $('#home_page').addClass("active");
+
+
     $('.filters').hide();
+    var filters = [];
+    $('label').click(function(){
+        var $tony = $(this).find('input');
+        if ($tony.is(':checked')){
+            console.log($tony.val());
+            filters.push($tony.val());
+            console.log(filters);
+        }
+        else {
+            var index = filters.indexOf($tony.val());
+            filters.splice(index, 1);
+            console.log(filters);
+        };
+    });
     $('#search_button').click(function(){
         var $input = $('#tftextinput');
         /* Check if there is valid input */
         if ($.trim($input.val()).length) {
             $('#loadingpage').show();
+        };
+        if (filters.length){
+
+            $('#storefilters').val(filters);
+            console.log($('#storefilters').val());
         };
     });
 
@@ -24,4 +45,5 @@ $(document).ready(function(){
         $('.filters').toggleClass('open');
         $('.row.content').toggleClass('open');
     });
+
 });
